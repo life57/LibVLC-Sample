0a9c798b168c6ee466c09cb67c23171110ea3b5a
life57 <42088083+life57@users.noreply.github.com>
2022-04-01 17:12:04 +0300

    Initial commit

---
+++ MainViewModel.cs
@@ -0,0 +1,86 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using LibVLCSharp.Shared;
+using LibVLCSharp.WPF;
+using System;
+
+namespace Stream1
+{
+    public enum PlayerState
+    {
+        Init,
+        Play,
+        Stop,
+        Pause
+    }
+    public class MainViewModel : ViewModelBase
+    {
+        private LibVLC libVLC;
+        public MediaPlayer mediaPlayer;
+        private Uri uri;
+        private PlayerState mediaState;
+        private string uriSource;
+        private bool showOnWpfWindow;
+
+        public MainViewModel()
+        {
+            libVLC = new LibVLC();
+            mediaPlayer = new MediaPlayer(libVLC);
+
+            mediaState = PlayerState.Init;
+            UriSource = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+            uri = new Uri(UriSource);
+            ShowOnWpfWindow = false;
+
+            PlayCommand = new RelayCommand(Play);
+            StopCommand = new RelayCommand(Stop);
+            PauseCommand = new RelayCommand(Pause);
+        }
+        public bool ShowOnWpfWindow
+        {
+            get => showOnWpfWindow;
+            set
+            {
+                showOnWpfWindow = value;
+                RaisePropertyChanged();
+            }
+        }
+        private void Pause()
+        {
+            mediaPlayer.Pause();
+            mediaState = PlayerState.Pause;
+        }
+
+        private void Stop()
+        {
+            mediaPlayer.Stop();
+            mediaState = PlayerState.Stop;
+        }
+
+        private void Play()
+        {
+            if (mediaState == PlayerState.Init)
+            {
+                mediaPlayer.Play(new Media(libVLC, uri));
+                mediaState = PlayerState.Play;
+            }
+            else
+            {
+                mediaPlayer.Play();
+            }
+        }
+        public string UriSource
+        {
+            get => uriSource;
+            set
+            {
+                uriSource = value;
+                RaisePropertyChanged();
+            }
+        }
+        public RelayCommand PlayCommand { get; set; }
+        public RelayCommand StopCommand { get; set; }
+        public RelayCommand PauseCommand { get; set; }
+        public VideoView MediaWindow { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using LibVLCSharp.Shared;
+using System.Windows;
+
+namespace Stream1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainViewModel vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Core.Initialize();
+            DataContextChanged += MainWindow_DataContextChanged;
+            vm = new MainViewModel();
+            DataContext = vm;
+        }
+
+        private void MainWindow_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(sender is MainWindow win)
+            {
+                if (vm.ShowOnWpfWindow)
+                    win.videoView.MediaPlayer = vm.mediaPlayer;
+            }
+        }
+    }
+}
